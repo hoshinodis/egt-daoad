cf88ec097bee872a870b05e4137e25e6b46dd602
koichirohatayama <koichiro_hatayama@adrea.jp>
2023-04-15 00:48:05 +0900

    feat: add tailwind config

--- webapp/front/tailwind.config.cjs
+++ webapp/front/tailwind.config.cjs
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          10: '#FFFFFF',
+          20: '#F5F6F5',
+          30: '#EEEEEE',
+          40: '#E1E3E2',
+          50: '#C4C6C5',
+          60: '#A1A5A3',
+          70: '#7A7F7C',
+          80: '#666C69',
+          90: '#48504C',
+          100: '#08120D',
+        },
+        primary: {
+          main: '#8EA99B',
+          10: '#E8EEEB',
+          20: '#C6D4CD',
+          30: '#768D81',
+          40: '#47554E',
+        },
+        secondary: '#F5BB5E',
+        passed: '#258034',
+        reject: '#8D0100',
+      },
+    },
+    fontFamily: {
+      sans: ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+    }
   },
   plugins: [],
 };
