1a4f51033773630860a18912c3f61a700b104a60
koichirohatayama <koichiro_hatayama@adrea.jp>
2023-04-15 02:18:41 +0900

    feat: add Card component

--- webapp/front/src/components/Elements/Card/index.tsx
+++ webapp/front/src/components/Elements/Card/index.tsx
@@ -5,5 +5,5 @@ export type CardPropsType = {
   children: React.ReactNode;
 };
 export const Card = ({ className, children }: CardPropsType) => (
-  <div className={clsx('p-8', className)}>{children}</div>
+  <div className={clsx('bg-[#678575]', className)}>{children}</div>
 );
