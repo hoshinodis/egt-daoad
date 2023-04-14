d1546ad029549023bf1fb5164f6ea51ca8481278
uhuko <okonomisentai@gmail.com>
2023-04-15 08:10:56 +0900

    chore: app

--- webapp/config/config.go
+++ webapp/config/config.go
@@ -5,8 +5,8 @@ import "github.com/caarlos0/env/v6"
 type Config struct {
 	Env        string `env:"APP_ENV" envDefault:"dev"`
 	Port       int    `env:"PORT" envDefault:"80"`
-	DBHost     string `env:"WEBAPP_DB_HOST" envDefault:"127.0.0.1"`
-	DBPort     int    `env:"WEBAPP_DB_PORT" envDefault:"13306"`
+	DBHost     string `env:"WEBAPP_DB_HOST" envDefault:"localhost"`
+	DBPort     int    `env:"WEBAPP_DB_PORT" envDefault:"3306"`
 	DBUser     string `env:"WEBAPP_DB_USER" envDefault:"root"`
 	DBPassword string `env:"WEBAPP_DB_PASSWORD" envDefault:"root"`
 	DBName     string `env:"WEBAPP_DB_NAME" envDefault:"webapp"`
