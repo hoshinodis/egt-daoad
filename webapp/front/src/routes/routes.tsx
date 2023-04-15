e8111e2e0629d759fc99c24b2b01e2b403c30710
koichirohatayama <koichiro_hatayama@adrea.jp>
2023-04-16 00:58:17 +0900

    fix: remove unused files

--- webapp/front/src/routes/routes.tsx
+++ webapp/front/src/routes/routes.tsx
@@ -3,7 +3,6 @@ import { RouteObject } from 'react-router-dom';
 import { lazyImport } from '@/utils/lazyImports';
 
 const { NotFound } = lazyImport(() => import('../features/NotFound'), 'NotFound');
-const { Top } = lazyImport(() => import('../features/Top'), 'Top');
 const { Advertiser } = lazyImport(() => import('../features/Advertiser'), 'Advertiser');
 const { Publisher } = lazyImport(() => import('../features/Publisher'), 'Publisher');
 const { Welcome } = lazyImport(() => import('../features/Welcome'), 'Welcome');
@@ -15,10 +14,6 @@ export const routes = [
   {
     path: '*',
     element: <NotFound />,
-  },
-  {
-    path: '/',
-    element: <Top />,
   },
   {
     path: '/advertiser',
